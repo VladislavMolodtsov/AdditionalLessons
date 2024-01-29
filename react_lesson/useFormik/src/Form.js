1438c3882b9bb01bac14ae5f70a90421446f2252
VladislavMolodtsov <vladi28061750@gmail.com>
2024-01-29 21:36:57 +0200

    using hook useField, code reduction, create several component for create input and checkbox. Second part

--- react_lesson/useFormik/src/Form.js
+++ react_lesson/useFormik/src/Form.js
@@ -1,146 +1,165 @@
-import React from "react";
-import {useFormik} from 'formik';
+import {Formik, Form, Field, ErrorMessage, useField} from 'formik';
 import * as Yup from 'yup';
 
-// В разных браузерах по разному отображаются разные виды встроенных валидаций
-// Далее мы бы хотели научится валидировать форму (поэтому мы напишем собственную ф-ию)
-// В validate мы будем передавать ф-ию по валидации каждаго интерактивного элемента, который есть у нас в форме
-// Данная ф-ия принимает значение values - это объект которой будет приходить из нашей формы
-// const validate = values => {
-//   const errors = {};
-//   console.log(values);
+// 12:58
+// Выводы ...
+// 1-ое: в реальных приложениях намного проше использовать компонент Formik
+// 2-ое: теперь мы знаем как он работает изнутри
+// 3-ье: валидацию намного проще проводить через сторонние библиотеки, а не прописывать свои большие условия
+// 4-ое: при помощи данной логики, мы можем валидировать любую форму
 
-//   if (!values.name) {
-//     errors.name = 'Обязательное поле';
-//   } else if (values.name.length < 2) {
-//     errors.name = 'Минимум 2 символа для заполнения';
-//   }
-
-//   if (!values.email) {
-//     errors.email = 'Обязательное поле';
-//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) { // Регулярные выражения.
-//     // Если вдруг по шаблону наш email не подошел, то мы...
-//     errors.email = 'Неправильный email адресс';
-//   }
-
-//   return errors;
-// }
-
-const Form = () => {
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      amount: 0,
-      currency: '',
-      text: '',
-      terms: false
-    },
-    // validate - это свойство useFormik, которое является ссылкой на функцию
-    // Данная ф-ия валидации будет срабатывать каждый раз, когда вызывается handleChange ...
-    // ... ввели мы что-то в input, у нас сработала ф-ия валидации, а также изменился state (initialValue)
-    // validate,
-
-    // validationSchema - это набор правил, которое мы можем применять к каждому с этих полей (тоесть initialValues)
-    // Yup.object - означает, что мы вернем объект с какими-то ключами
-    // Chaining - это набор команд в цепочке
-    validationSchema: Yup.object({
-      name: Yup.string()
-              .min(2, 'Минимум 2 символа!')
-              .required('Обязательное поле!'),
-      email: Yup.string()
-              .email('Неправильный email адрес!')
-              .required('Обязательное поле!'),
-      amount: Yup.number()
-              .min(5, 'Не менее 5')
-              .required('Обязательное поле!'),
-      currency: Yup.string().required('Выберите валюту'),
-      text: Yup.string()
-              .min(10, 'Не менее 10 символов'),
-      terms: Yup.boolean()
-              .required('Необходимо согласие')
-              .oneOf([true], 'Необходимо согласие')
-    }),
-    onSubmit: values => console.log(JSON.stringify(values, null, 2))
-  })
+const MyTextInput = ({label, ...props}) => {
+  // Передаем пропсы, затем мы их диструктурируем в props используем spread operator. label - это текст внутри label, ...props - это attr компонента field
+  const [field, meta] = useField(props);
+  // useField позволяет получать массив из 2-х объектов
+  // field - это props-ы, value, onBlur, onChange которые отвечают за текущее состояние нашего input
+  // meta - это метаданные с ошибками и был ли уже использован данный input
+  // В useField вторым аргументом можем использовать тип аргумента который мы будем испльзовать (checkbox, select)
 
   return (
-    <form className="form" onSubmit={formik.handleSubmit}>
-      <h2>Отправить пожертвование</h2>
+    <>
+      <label htmlFor={props.name}>{label}</label>
+      <input {...props} {...field}/>
+      {/* В данный input мы должны поместить все props (id, name, type и др.) для этого мы помещаем их в конструкцию {...props} */}
+      {/* Теперь нам нужно связать наш компонент с нашей формой Formik - это значит, что наш input должен получить такие характеристики как ... */}
+      {/* ...  value, onBlur, onChange - для этого соществует hook useField, он через контекст будет получать эти property, когда он используется внутри Formik-a*/}
+      {meta.touched && meta.error ? <div className='error'>{meta.error}</div> : null}
+      {/* .touched - использовал ли input наш пользователь */}
+      {/* .error - описание ошибки */}
+    </>
+  )
+}
 
-      <label htmlFor="name">Ваше имя</label>
-      <input 
-        id="name"
-        name="name" 
-        type="text" 
-        value={formik.values.name} 
-        onChange={formik.handleChange} 
-        onBlur={formik.handleBlur}/>
-        {/* onBlur - это событие срабатывает, когда мы уводим фокус с элемента (противоположное событие называется onFocus (когда нажымаем на элемент)) */}
-        {/* Когда пользователь проивзаимодействовал с input срабатывает метод onBlur и свойство name на input записывается в объект touch , теперь мы... */}
-        {/* ... мы можем правильно отображать сообщения с ошибкой ( func validate), а не когда при вводе срабатывают все input */}
-        {formik.errors.name && formik.touched.name ? <div className='error'>{formik.errors.name}</div> : null}
-        {/*  Мы обращается к формику, к его методу с ошибками */}
-        {/* Если в поле input будет ошибка, то мы рэндерим (то что было указано в ф-ии validate (в условиях))... */}
-        {/* ... и пользователь уже полностью провзаимодействовал с input, то только тогда мы выводим сообщение */}
-
-      <label htmlFor="email">Ваша почта</label>
-      <input 
-        id="email"
-        name="email" 
-        type="email" 
-        value={formik.values.email} 
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}/>
-      {formik.errors.email && formik.touched.email ? <div className='error'>{formik.errors.email}</div> : null}
-
-      <label htmlFor="amount">Количество</label>
-      <input 
-        id="amount" 
-        name="amount"
-        type="number"
-        value={formik.values.amount}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}/>
-      {formik.errors.amount && formik.touched.amount ? <div className='error'>{formik.errors.amount}</div> : null}
-
-      <label htmlFor="currency">Валюта</label>
-      <select 
-        id="currency"
-        name="currency"
-        value={formik.values.currency}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}>
-          <option value="">Выберите валюту</option>
-          <option value="USD">USD</option>
-          <option value="UAH">UAH</option>
-          <option value="RUB">RUB</option>
-      </select>
-      {formik.errors.currency && formik.touched.currency ? <div className='error'>{formik.errors.currency}</div> : null}
-
-      <label htmlFor="text">Ваше сообщение</label>
-      <textarea
-        id="text"
-        name="text"
-        value={formik.values.text}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}/>
-      {formik.errors.text && formik.touched.text ? <div className='error'>{formik.errors.text}</div> : null}
-
-      <label className="checkbox">
-        <input
-          name="terms"
-          type="checkbox"
-          value={formik.values.name}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}/>
-            Соглашаетесь с политикой конфиденциальности?
+const MyCheckbox = ({children, ...props}) => {
+  // Передаем пропсы, затем мы их диструктурируем в props используем spread operator
+    // children - это необезательно текст, это может быть компонент или что-то еще
+    // 
+    // В useField вторым аргументом можем использовать тип аргумента который мы будем испльзовать (checkbox, select)
+    // за счет данногo type у нас в field прийдет не value а checked значение, которые мы меняем как пользователь
+  const [field, meta] = useField({...props, type: 'checkbox'});
+  return (
+    <>
+      <label className='checkbox'>
+        <input type="checkbox" name="terms" {...props} {...field}/>
+            {children}
       </label>
-      {formik.errors.terms && formik.touched.terms ? <div className='error'>{formik.errors.terms}</div> : null}
+      {meta.touched && meta.error ? <div className='error'>{meta.error}</div> : null}
+    </>
+  )
+}
 
-      <button type="submit">Отправить</button>
-    </form>
+const CustomForm = () => {
+  return (
+    <Formik
+      // initialValues - теперь это properties, а не объект, все что внутри мы должны окружить фигурными скобками
+      initialValues = {{
+        name: '',
+        email: '',
+        amount: 0,
+        currency: '',
+        text: '',
+        terms: false
+      }}
+      validationSchema = {Yup.object({
+        name: Yup.string()
+                .min(2, 'Минимум 2 символа!')
+                .required('Обязательное поле!'),
+        email: Yup.string()
+                .email('Неправильный email адрес!')
+                .required('Обязательное поле!'),
+        amount: Yup.number()
+                .min(5, 'Не менее 5')
+                .required('Обязательное поле!'),
+        currency: Yup.string().required('Выберите валюту'),
+        text: Yup.string()
+                .min(10, 'Не менее 10 символов'),
+        terms: Yup.boolean()
+                .required('Необходимо согласие')
+                .oneOf([true], 'Необходимо согласие')
+      })}
+      onSubmit = {values => console.log(JSON.stringify(values, null, 2))}
+    >
+      <Form className="form">
+        <h2>Отправить пожертвование</h2>
+
+        {/* <label htmlFor="name">Ваше имя</label> */}
+        {/* <Field 
+          // Компонент Field получает props value, handleChange, handleBlur - через контекст компонента Form 
+          id="name"
+          name="name" 
+          type="text"/> */}
+        {/* <ErrorMessage className="error" name='name' component='div'/> */}
+        {/* component - мы указываем потому что без него наш компонент ErrorMessage будет рендериться в формате строки, а с данным компонентом у нас будет рендериться div внутри которого будет сообщение об ошибке, который приходит от validationSchema*/}
+
+        <MyTextInput
+          label="Ваше имя"
+          id="name"
+          name="name" 
+          type="text"/>
+
+        {/* <label htmlFor="email">Ваша почта</label>
+        <Field 
+          id="email"
+          name="email" 
+          type="email"/>
+        <ErrorMessage className="error" name='email' component='div'/> */}
+
+        <MyTextInput
+          label="Ваша почта"
+          id="email"
+          name="email" 
+          type="email"/>
+
+        {/* <label htmlFor="amount">Количество</label>
+        <Field
+          id="amount" 
+          name="amount"
+          type="number"/>
+        <ErrorMessage className="error" name='amount' component='div'/> */}
+
+        <MyTextInput
+          label="Количество"
+          id="amount" 
+          name="amount"
+          type="number"/>
+
+        <label htmlFor="currency">Валюта</label>
+        <Field 
+          id="currency"
+          name="currency"
+          // Field по умолчанию рендерит input, поэтому нам нужно указать аттрибут as, для того чтобы указать какой ...
+          // ... какой компонент нам нужно отрендерить. Мы рендерим Field в качестве select
+          as='select'>
+            <option value="">Выберите валюту</option>
+            <option value="USD">USD</option>
+            <option value="UAH">UAH</option>
+            <option value="RUB">RUB</option>
+        </Field>
+        <ErrorMessage className="error" name='currency' component='div'/>
+
+        <label htmlFor="text">Ваше сообщение</label>
+        <Field
+          id="text"
+          name="text"
+          as='textarea'/>
+        <ErrorMessage className="error" name='text' component='div'/>
+
+        {/* <label className="checkbox">
+          <Field
+            name="terms"
+            type="checkbox"/>
+              Соглашаетесь с политикой конфиденциальности?
+        </label>
+        <ErrorMessage className="error" name='terms' component='div'/> */}
+        <MyCheckbox
+          name="terms">
+          Соглашайтесь с политикой конфиденциальности?
+        </MyCheckbox>
+
+        <button type="submit">Отправить</button>
+      </Form>
+    </Formik>
   );
 };
 
-export default Form;
+export default CustomForm;
